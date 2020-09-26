b032c2697788601dcad11dd6cdc6530407204a3d
Mohsin Tariq <mohsin.tariq@capgemini.com>
2020-09-26 10:17:12 +0100

    NPM Scripts Part 1

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+        } else if ($("#carouselButton").children("span").hasClass("fa-play")) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+        }
+    });
+
+    $("#loginButton").click(function () {
+        $('#loginModal').modal({
+            backdrop: 'static',
+            keyboard: true,
+            show: true
+        });
+    });
+    $("#reserveButton").click(function () {
+        $('#reserveTable').modal({
+            backdrop: 'static',
+            keyboard: true,
+            show: true
+        });
+    });
+
+    $(".cancel-login-modal").click(function () {
+        $('#loginModal').modal('hide');
+    });
+
+    $(".cancel-res-modal").click(function () {
+        $('#reserveTable').modal('hide');
+    });
+});
